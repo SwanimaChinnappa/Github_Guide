1d4564aebe2ddb91043837599fdcc2cfe7763e59
SwanimaChinnappa <99345170+SwanimaChinnappa@users.noreply.github.com>
2022-03-28 17:43:05 -0400

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,9 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-first-bucket"
+resource "aws_s3_bucket" "myfirstbucket" {
+  bucket = "myfirstbucket20220328"
+  acl    = var.acl_type
+  tags = {
+    createdby   = "Swanima C"
+    Environment = "Dev"
+    For         = "Onboarding-Purpose"
   }
+}
